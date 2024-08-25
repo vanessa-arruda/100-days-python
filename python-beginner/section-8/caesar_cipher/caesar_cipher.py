6c13b81a14d231416040b92ba4659fc903d1a487
Vanessa Arruda <adm.vanessasouza@gmail.com>
2024-08-25 22:49:39 +0200

    Fix: Add modulo operation to handle shift values exceeding alphabet length
    Feat: Add loop to main() for repeated operations to improve ux

--- python-beginner/section-8/caesar_cipher/caesar_cipher.py
+++ python-beginner/section-8/caesar_cipher/caesar_cipher.py
@@ -1,20 +1,31 @@
 from caesar_data import logo
-from caesar_functions import encrypt_decrypt
+from caesar_function import encrypt_decrypt
+
+print(logo)
 
 
 def main():
-    print(logo)
 
-    user_choice = str(
-        input("Type 'encode' to encrypt, type 'decode' to decrypt: ").lower()
-    )
+    while True:
 
-    if user_choice == "encode" or user_choice == "decode":
-        user_text = str(input(f"Type your message to be {user_choice}d: ").lower())
-        shift_number = int(input("Type the shift number: "))
-        print(encrypt_decrypt(user_choice, user_text, shift_number))
-    else:
-        print("Option not found.")
+        user_choice = str(
+            input("Type 'encode' to encrypt, type 'decode' to decrypt: ").lower()
+        )
+
+        if user_choice == "encode" or user_choice == "decode":
+            user_text = str(input(f"Type your message to be {user_choice}d: ").lower())
+            shift_number = int(input("Type the shift number: "))
+            print(encrypt_decrypt(user_choice, user_text, shift_number))
+        else:
+            print("Option not found. Goodbye!")
+            break
+
+        restart = str(
+            input("Do you want to make another check? Type 'yes' or 'no': ").lower()
+        )
+        if restart != "yes":
+            print("Goodbye!")
+            break
 
 
 main()
--- python-beginner/section-8/caesar_cipher/caesar_function.py
+++ python-beginner/section-8/caesar_cipher/caesar_function.py
@@ -2,28 +2,27 @@ from caesar_data import alphabet
 
 
 def encrypt_decrypt(user_choice: str, text: str, shift: int) -> str:
-    text_list = text.split()
-    new_text = []
-
-    # print(f"innitial text: {text}")  # output "text"
-    # print(f"innitial text list: {text_list}")  # output [text, ]
+    text_list: list[str] = text.split()
+    new_text: list[str] = []
 
     for word in text_list:
-        new_word = ""
+        new_word: str = ""
         for letter in word:
             if letter in alphabet:
                 if user_choice == "encode":
                     # get the index number of the equivalent letter + shifts needed to make
-                    new_letter = alphabet[(alphabet).index(letter) + shift]
+                    new_letter: str = alphabet[
+                        (alphabet.index(letter) + shift) % len(alphabet)
+                    ]
                     new_word += new_letter
                 elif user_choice == "decode":
-                    new_letter = alphabet[(alphabet).index(letter) - shift]
+                    new_letter = alphabet[
+                        (alphabet.index(letter) - shift) % len(alphabet)
+                    ]
                     new_word += new_letter
             else:
                 new_word += letter
         new_text.append(new_word)
-        # print(f"encrypted: {new_word}")
-        # print(f"new text: {new_text}")
 
     final_text = " ".join(new_text)  # turn back into string
     if user_choice == "encode":
